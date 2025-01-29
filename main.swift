f4ae4b5446ce8f5a991102fca3391853753ca807
Patreece Eichel <patreece.eichel@outlook.com>
2025-01-29 15:00:21 +0000

    Balance class

--- main.swift
+++ main.swift
@@ -2,4 +2,29 @@
 // Blackjack
 // Patreece Eichel & John Lazaro
 
+public struct Balance : CustomStringConvertible 
+{
+  private var balance : Double = 100.00; 
+
+  // Retrieves the balance
+  public func getBalance() -> Double 
+  {
+    return balance;
+  }
+
+  // Reduces the balance by the provided amount
+  public func reduceBalance(amount: Double) 
+  {
+    balance -= amount;
+  }
+
+  // Increases the balance by the provided amount
+  public func increaseBalance(amount: Double)
+  {
+    balance += amount;
+  }
+}
+
+
+print("Welcome to Blackjack!\n");
 
